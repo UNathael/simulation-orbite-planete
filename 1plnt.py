8a63541677d35f0a455b6d133697e666f8643c40
UNathael <unathael@gmail.com>
2026-03-28 13:37:35 -1000

    ajout du script de simulation

---
+++ 1plnt.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Éditeur de Spyder
+
+Ceci est un script temporaire.
+"""
+import math
+import matplotlib.pyplot as plt
+
+#définition des constantes
+G=1 #6.67*(10**11)
+mpl = 1 #6*10**24
+dt=0.01
+
+#terre
+#position de base
+px = 1#1.5*(10**8)
+py = 0
+
+#vitesse de base
+vx = 0
+vy = 1 
+
+
+#calcule de la distance solei(x=o; y=0)/centre de la terre 
+D = math.sqrt(px**2 + py**2)
+
+#accélération
+a = G*mpl/(D**2)
+
+
+
+plt.ion()
+fig, ax = plt.subplots()
+
+for i in range(5000):
+    ax = -a*px
+    ay = -a*py
+    
+    vx = vx + ax *dt
+    vy = vy +ay *dt
+    
+    px = px + vx * dt
+    py = py + vy * dt
+    
+    
+    plt.clf()
+    
+    plt.scatter(0,0, color="red")
+    plt.scatter(px, py, color="blue")
+    
+    plt.xlim(-2, 2)
+    plt.ylim(-2,2)
+    
+    plt.pause(0.001)
+    
+    plt.ioff()
+    plt.show()
