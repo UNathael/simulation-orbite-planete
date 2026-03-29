ce86d8befadca7199c75d251c9d9b151ede3ca96
UNathael <unathael@gmail.com>
2026-03-28 18:33:25 -1000

    Update 1plnt.py

--- 1plnt.py
+++ 1plnt.py
@@ -9,32 +9,29 @@ import matplotlib.pyplot as plt
 
 #définition des constantes
 G=1 #6.67*(10**11)
-mpl = 1 #6*10**24
+mpl = 1 #6*10**24normalemnt mais 1 pour simplifier
 dt=0.01
 
 #terre
 #position de base
-px = 1#1.5*(10**8)
+px = 1#1.5*(10**8)normalement mais 1 pour simplifier
 py = 0
 
 #vitesse de base
 vx = 0
 vy = 1 
 
-
-#calcule de la distance solei(x=o; y=0)/centre de la terre 
-D = math.sqrt(px**2 + py**2)
-
-
-
-
-
 plt.ion()
 fig, ax = plt.subplots()
 
 for i in range(5000):
+    
+    #calcule de la distance solei(x=o; y=0)/centre de la terre 
+    D = math.sqrt(px**2 + py**2)
+   
     #accélération
     a = G*mpl/(D**2)
+    
     ax = -a*px
     ay = -a*py
     
